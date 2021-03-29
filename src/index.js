f4726a47e49f28f5c8397f3ad0b2e40868d8447c
Dongkyun Kim <s98101000@gmail.com>
2021-03-29 15:42:48 +0900

    a2hs test

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <button class="add-button">Add to home screen</button>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -21,3 +22,28 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+});
+addBtn.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+  addBtn.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
